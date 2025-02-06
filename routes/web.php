e6eece8ca07176075050cfe1dee7a735277ddd7e
NIK MUHAMMAD ASYRAF <nikasyraf2001@gmail.com>
2025-02-06 16:38:45 +0800

    Open AI PHP Client

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Profile\AvatarController;
+use OpenAI\Laravel\Facades\OpenAI;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -33,3 +35,15 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/openai', function() {
+
+    $result = OpenAI::chat()->create([
+        'model' => 'gpt-3.5-turbo',
+        'messages' => [
+            ['role' => 'user', 'content' => 'Hello!'],
+        ],
+    ]);
+
+echo $result->choices[0]->message->content; // Hello! How can I assist you today?
+});
