4cb875a9de64737dd509988bc805b2a8aa1d2b60
NIK MUHAMMAD ASYRAF <nikasyraf2001@gmail.com>
2025-02-06 10:28:35 +0800

    feat: add logic to delete the old avatar file before adding new file

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers\Profile;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\UpdateAvatarRequest;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\UpdateAvatarRequest;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request){
 
-        $path = ($request->file('avatar')->store('avatars', 'public'));
+        // $path = $request->file('avatar')->store('avatars', 'public');
+        $path = Storage::disk('public')->put('avatars', $request->file('avatar'));
+
+        if($oldAvatar = $request->user()->avatar) {
+            Storage::disk('public')->delete($oldAvatar);
+        }
         auth()->user()->update(['avatar' => $path]);
         // dd(auth()->user());
 
